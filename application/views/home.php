ed500bf33971250a1e690306f3c8f20da0ac78bc
shen jian <shenjian@juyide.com>
2013-03-04 15:15:42 +0800

    tip

--- application/views/home.php
+++ application/views/home.php
@@ -3,7 +3,7 @@
     <ul id="da-thumbs" class="da-thumbs clearfix">
         <?php foreach($brand as $row): ?>
         <li>
-            <a href="#" data-toggle="popover" data-trigger="hover" data-title="<?=$row->cnname?>" data-content="<?php echo $row->description.'----[产地] '.$row->field?>" data-placement="bottom" data-delay="{ show: 500, hide: 100 }">
+            <a href="#" data-toggle="popover" data-title="<?=$row->cnname?>" data-content="<?php echo $row->description.'----[产地] '.$row->field?>" data-placement="bottom" data-delay="{ show: 500, hide: 100 }">
                 <img src="<?=base_url().'/uploads/'.$row->img?>" />
                 <div><span><?=$row->cnname?></span></div>
             </a>
